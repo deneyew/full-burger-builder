dcc840bdace86919651bfa781fd9a8099b14eb49
danieleneyew <deneyew@gmail.com>
2021-03-17 16:58:58 -0700

    First commit

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+//import Input from '../../components/UI/Input/Input';
+//import Button from '../../components/UI/Button/Button';
+import classes from './Auth.module.css'
+import * as actions from '../../store/actions/index';
+
+
+/* 
+ * const mapDispatchToProps = dispatch => {
+ *      return {
+ *          onAuth: (email, password) => dispatch(actions.auth(email, password))
+ *      };
+ * }; 
+ */
+// export default connect(null, mapDispatchToProps)(Auth);
