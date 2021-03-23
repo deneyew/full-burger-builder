1f75e20729f85bc906d9419aeb6f35f6c1cf31ea
danieleneyew <deneyew@gmail.com>
2021-03-22 21:01:18 -0700

    Authentication Finished, lazy loading added and console logs removed

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,11 +4,7 @@ import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-    // This could be a functional component, doesn't have to be a class
-    componentWillUpdate() {
-        console.log('[OrderSummary] WillUpdate');
-    }
-
+    // This could be a functional component, doesn't have to be a class since we just render something
     render () {
         const ingredientSummary = Object.keys( this.props.ingredients )
             .map( igKey => {
